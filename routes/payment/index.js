66b057b0db02b41f4ca5ff938f49cdd87330e838
anshul46 <89180266+AnshB0712@users.noreply.github.com>
2023-01-10 13:59:45 +0530

    m

--- routes/payment/index.js
+++ routes/payment/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const paymentRouter = express.Router()
 const paymentController = require('../../controllers/payment/paymentController');
-const paymentProcess = require('../../controllers/payment/paymentProcess');
 const paymentStatusController = require('../../controllers/payment/paymentStatusController');
 const changePaymentStatus = require('../../controllers/payment/changePaymentStatus');
 
 paymentRouter
 .post('/paytm',paymentController)
-.post('/paytm/process',paymentProcess)
 .post('/paytm-status',paymentStatusController)
 .patch('/paytm/change-status',changePaymentStatus)
 
