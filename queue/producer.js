e2d89178fbdf761a19d605e4127fa4e04b9bf8ce
Anshul Borde <89180266+AnshB0712@users.noreply.github.com>
2023-01-31 10:47:12 +0530

    Update producer.js

--- queue/producer.js
+++ queue/producer.js
@@ -5,6 +5,7 @@ const publishToQueue = async (data) => {
         console.log(data)
         await channels.payment.sendToQueue('payment-stag',Buffer.from(JSON.stringify(data)))
     } catch (error) {
+        console.log(error)
         throw new Error('--- Q-Publisher Error ---')
     }
 }
